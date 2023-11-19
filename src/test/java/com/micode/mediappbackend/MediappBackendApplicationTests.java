545d1942d0089da883f0fe2ed9aef37c6f021754
Pedro Delgado Aparicio <pedro.delgado.aparicio@gmail.com>
2023-11-19 08:29:12 -0500

    first commit

---
+++ src/test/java/com/micode/mediappbackend/MediappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micode.mediappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
